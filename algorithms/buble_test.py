ce1ce690efb3f7cef4694e2ffe3a32075f6d3c78
dmitry-uraev <uraev.dima@yandex.ru>
2019-10-20 14:53:28 +0300

    'first;

---
+++ algorithms/buble_test.py
@@ -0,0 +1,14 @@
+"""
+tests...
+"""
+
+import unittest
+from algorithms.main import Sorter 
+
+
+class SorterCheck(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ algorithms/main.py
@@ -0,0 +1,8 @@
+"""
+A short implementation of some sorting algorithms...
+"""
+
+
+class Sorter:
+    def __init__(self):
+        pass
